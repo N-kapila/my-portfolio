36497372d26d4493e21d3e22a2f8e36e7e8dfb7a
N-kapila <nirmalkapilarathne1998@gmail.com>
2022-08-15 22:38:03 +0530

    initial commit

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div className="profile-container">
+        <div className="profile-parent">
+            <div className="profile-details">
+                <div className="colz">
+                    <a href='https://www.facebook.com/nkmaxx.k'>
+                        <i className='fa fa-facebook-square'></i>
+                    </a>
+                    <a href='https://www.instagram.com/_i_am_nim_z_/'>
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href='https://github.com/N-kapila'>
+                        <i className='fa fa-github'></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/nirmal-kapilarathne-79b7331a1/'>
+                        <i className='fa fa-linkedin'></i>
+                    </a>
+                    <a href='https://twitter.com/NKapilarathne'>
+                        <i className='fa fa-twitter'></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
